a41b62a0ade9e900ec51552860948856330bf2c0
zee <support@z-mirror.com>
2025-12-20 12:31:52 +0530

    fx

--- bot/helper/ext_utils/files_utils.py
+++ bot/helper/ext_utils/files_utils.py
@@ -131,8 +131,8 @@ async def clean_download(opath):
 
 
 async def clean_all():
-    await TorrentManager.remove_all()
-    LOGGER.info("Cleaning Download Directory")
+    LOGGER.info("Cleaning Download Directory without TorrentManager")
+    # Directly clean the DOWNLOAD_DIR without touching TorrentManager
     await (await create_subprocess_exec("rm", "-rf", DOWNLOAD_DIR)).wait()
     await aiomakedirs(DOWNLOAD_DIR, exist_ok=True)
 
